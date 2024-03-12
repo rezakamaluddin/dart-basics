b63b73538876a1843e6a242dd2d9e3d7b5c3a9cb
rezakamaluddin <108996565+rezakamaluddin@users.noreply.github.com>
2024-03-12 14:47:01 +0700

    10_konversi_string

---
+++ dart_project/bin/latihan/10_konversi_string.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  String angkaString = '123';
+
+  // Menggunakan fungsi int.parse untuk mengonversikan String menjadi int
+  int angkaInt = int.parse(angkaString);
+
+  // cetak hasil konversi
+  print("String: $angkaString");
+  print("Integer: $angkaInt");
+}
