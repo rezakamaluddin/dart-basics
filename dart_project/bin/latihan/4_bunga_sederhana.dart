2005de608a63238eb891dfa645eb3ff78ebf1839
rezakamaluddin <108996565+rezakamaluddin@users.noreply.github.com>
2024-03-12 14:38:26 +0700

    4_bunga_sederhana

---
+++ dart_project/bin/latihan/4_bunga_sederhana.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  double p = 50;
+  double t = 2;
+  double r = 5;
+
+  double bungaSederhana = (p * t * r) / 100;
+
+  print('Bunga sederhana adalah : $bungaSederhana');
+}
